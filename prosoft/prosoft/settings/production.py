d8c3b5dd423095635e8ada6c2dd727824a4dabac
CLAUDIO ROMAN <CLAUDIO.ROMAN@EEOC.GOV>
2019-03-29 15:22:15 -0400

    database 2 in production

--- prosoft/prosoft/settings/production.py
+++ prosoft/prosoft/settings/production.py
@@ -10,10 +10,11 @@ INSTALLED_APPS += (
 )
 
 import dj_database_url
+#db_from_env = dj_database_url.config()
 DATABASES = {
 	'default': dj_database_url.config()
 }
-db_from_env = dj_database_url.config(conn_max_age==500)
+db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
 
 STATICFILES_DIRS = (
