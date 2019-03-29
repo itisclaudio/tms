f507f881d7b8d897b2c25b97ef908885bdc4175a
CLAUDIO ROMAN <CLAUDIO.ROMAN@EEOC.GOV>
2019-03-29 15:29:47 -0400

    whitenoise added

--- prosoft/prosoft/settings/production.py
+++ prosoft/prosoft/settings/production.py
@@ -9,6 +9,19 @@ INSTALLED_APPS += (
 	'django.contrib.admin',
 )
 
+MIDDLEWARE_CLASSES = (
+	'whitenoise.middleware.WhiteNoiseMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'prosoft.libs.middleware.LoginRequiredMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
+)
+
 import dj_database_url
 #db_from_env = dj_database_url.config()
 DATABASES = {
