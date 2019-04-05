214d0c83378036a827652a0926b5c19b4b75c5d5
CLAUDIO ROMAN <CLAUDIO.ROMAN@EEOC.GOV>
2019-04-05 16:21:13 -0400

    STATICFILES_DIR

--- prosoft/settings/base.py
+++ prosoft/settings/base.py
@@ -3,8 +3,8 @@
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 
-#BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+#BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print "in base.py, BASE_DIR:"
 print BASE_DIR
 
