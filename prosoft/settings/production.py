3ffa0c010041e10adb2dfcb3ad990e32219bfc47
claudio roman <claudio.roman@eeoc.gov>
2019-04-26 14:20:57 -0400

    Amazon 1 - Requirements and settings

--- prosoft/settings/production.py
+++ prosoft/settings/production.py
@@ -7,6 +7,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['prosoft-tms.herokuapp.com']
 INSTALLED_APPS += (
 	'django.contrib.admin',
+	'storages',#App needed for Amazon AWS S3
 )
 
 # SECURITY WARNING: keep the secret key used in production secret!
@@ -48,6 +49,14 @@ LOCAL_DEV = False
 
 #Heroku variable for https:
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+#For Amazon S3
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NANE = os.environ.get('AWS_STORAGE_BUCKET_NANE')
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 #To send emails
 EMAIL_USE_TLS = True
