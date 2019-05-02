7a0c10291f1c0cd6fcdd82145ea2e9c416ed5fdb
claudio roman <claudio.roman@eeoc.gov>
2019-05-02 12:46:49 -0400

    base.py now with github

--- prosoft/settings/base.py
+++ prosoft/settings/base.py
@@ -5,8 +5,6 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 #BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-print "in base.py, BASE_DIR:"
-print BASE_DIR
 
 DEBUG = False
 
@@ -40,15 +38,15 @@ INSTALLED_APPS = (
 )
 
 MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
+	'django.contrib.sessions.middleware.SessionMiddleware',
+	'django.middleware.common.CommonMiddleware',
+	'django.middleware.csrf.CsrfViewMiddleware',
+	'django.contrib.auth.middleware.AuthenticationMiddleware',
 	'prosoft.libs.middleware.LoginRequiredMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
+	'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
+	'django.contrib.messages.middleware.MessageMiddleware',
+	'django.middleware.clickjacking.XFrameOptionsMiddleware',
+	'django.middleware.security.SecurityMiddleware',
 )
 
 ROOT_URLCONF = 'prosoft.urls'
