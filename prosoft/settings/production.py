5610924a2f0a84d7d01cbc8799b47c477cf47499
claudio roman <claudio.roman@eeoc.gov>
2019-09-26 16:50:12 -0400

    New middleware to load glyphicons font 6

--- prosoft/settings/production.py
+++ prosoft/settings/production.py
@@ -14,7 +14,6 @@ SECRET_KEY = os.environ.get('SECRET_KEY','afl549q&v2eppw3reii)7wozdiol47n)hr7^fi
 
 MIDDLEWARE_CLASSES = (
 	'django.contrib.sessions.middleware.SessionMiddleware',
-	'prosoft.libs.middleware.AddControlToHeader',#To allow AWS to serve fonts
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -24,7 +23,7 @@ MIDDLEWARE_CLASSES = (
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 	'django.middleware.security.SecurityMiddleware',
 )
-
+#'prosoft.libs.middleware.AddControlToHeader',#To allow AWS to serve fonts
 import dj_database_url
 #db_from_env = dj_database_url.config()
 DATABASES = {
